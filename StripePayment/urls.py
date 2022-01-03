5316f678ca9a4aa7cc18613676ccd2f954538c70
Aniveshnishad76 <anivesh.nishad76@gmail.com>
2022-01-03 16:11:07 +0530

    changes

--- StripePayment/urls.py
+++ StripePayment/urls.py
@@ -7,7 +7,7 @@ router = routers.SimpleRouter(trailing_slash=False)
 router.register(r'payment/', stripe_views.StripePayment, basename='payment'),
 # router.register(r'repayment-classes/', stripe_views.RepaymentClasses, basename='repayment_classes'),
 urlpatterns = [
-    path("create-payment-intent/", stripe_views.createpayment),
+    path("create-payment-intent", stripe_views.createpayment),
     path('repayment-classes/', stripe_views.RepaymentClasses.as_view(), name="repayment_classes"),
 
 ]
